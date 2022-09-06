213b994e3e15cebfe3c4c42bcfc0d19e56193dbc
Samuellekpor <samuellekpor@gmail.com>
2022-09-06 15:15:58 +0000

    Correct errors in css file and linters

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let toggleBtn = document.querySelector('.toggle-btn');
-let navBarList = document.querySelector('.nav-bar-list');
-let navBar = document.querySelector('.nav-bar');
+const toggleBtn = document.querySelector('.toggle-btn');
+const navBarList = document.querySelector('.nav-bar-list');
+const navBar = document.querySelector('.nav-bar');
 
-toggleBtn.addEventListener('click', function(event){
+toggleBtn.addEventListener('click', (event) => {
   event.preventDefault();
   toggleBtn.classList.toggle('active');
   navBarList.classList.toggle('active');
   navBar.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-bar-list>li>a").forEach(n => n.addEventListener("click",function(){
-  toggleBtn.classList.remove("active");
-  navBarList.classList.remove("active");
+document.querySelectorAll('.nav-bar-list>li>a').forEach((n) => n.addEventListener('click', () => {
+  toggleBtn.classList.remove('active');
+  navBarList.classList.remove('active');
   navBar.classList.remove('active');
-}))
+}));
